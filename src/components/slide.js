f3b03fe4f976af5b3d4b111dff043637e6686cdf
Ricardo José <rjlopes@uolinc.com>
2020-05-16 14:19:22 -0300

    change slide to component to prevent SSR error

---
+++ src/components/slide.js
@@ -0,0 +1,49 @@
+import React from "react"
+
+let src;
+
+class SlideComponent extends React.PureComponent{
+
+    constructor(props){
+        super(props);
+
+        src = this.props.src || 'https://slides.com/zericardo/teste/embed';
+
+        
+        this.state = {
+            width: window.screen.width,
+            height: window.screen.height + 39
+        };
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.resize.bind(this));
+        this.resize();
+    }
+    
+    resize() {
+        
+        this.setState({
+            width: window.screen.width,
+            height: window.screen.height + 39
+        });
+        
+    }
+
+    render(){
+        return (
+            <div>
+
+            <iframe 
+            src={src} 
+            width={this.state.width} height={this.state.height} 
+            scrolling="no" 
+            allowFullScreen></iframe>
+
+            </div>
+        )
+    }
+}
+
+
+export default SlideComponent
--- src/pages/slide.js
+++ src/pages/slide.js
@@ -1,51 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-let src;
+import SlideComponent from "../components/slide"
 
-class slidePage extends React.PureComponent{
-
-    constructor(props){
-        super(props);
-
-        src = this.props.src || 'https://slides.com/zericardo/teste/embed';
-
-        
-        this.state = {
-            width: window.screen.width,
-            height: window.screen.height + 39
-        };
-    }
-
-    componentDidMount() {
-        window.addEventListener("resize", this.resize.bind(this));
-        this.resize();
-    }
-    
-    resize() {
-        
-        this.setState({
-            width: window.screen.width,
-            height: window.screen.height + 39
-        });
-        
-    }
-
-    render(){
-        return (
-            <div>
-
-            <iframe 
-            src={src} 
-            width={this.state.width} height={this.state.height} 
-            scrolling="no" 
-            allowFullScreen></iframe>
-
-            </div>
-        )
-    }
-}
-
+const slidePage = () => (<SlideComponent/>)
 
 export default slidePage
