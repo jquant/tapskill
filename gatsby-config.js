14de9daf3246531bb40ec41bff7e27f8cbd98982
Ricardo José <rjlopes@uolinc.com>
2020-05-25 20:24:12 -0300

    fix favicon path

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `public/tapLogo.svg`, // This path is relative to the root of the site.
+        icon: `static/tapLogo.svg`, // This path is relative to the root of the site.
       },
     },
     {
