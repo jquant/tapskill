d855de37125e7e86b1f6e93237df63acf7b3b213
Ricardo José <rjlopes@uolinc.com>
2020-05-25 20:13:46 -0300

    fix static image path

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/images`,
+        path: `${__dirname}/src/images`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `images/tapLogo.svg`, // This path is relative to the root of the site.
+        icon: `public/tapLogo.svg`, // This path is relative to the root of the site.
       },
     },
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 // import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -10,17 +10,11 @@ const IndexPage = () => (
     <div className="h-screen pb-14 bg-right bg-cover" style={{backgroundImage: 'url("bg.svg")'}}>
         <div className="container pt-24 md:pt-32 px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
           <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
-            <h1 className="my-4 text-3xl md:text-5xl text-purple-800 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Main Hero Message to sell your app</h1>
-            <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">Sub-hero message, not too long and not too short. Make it just right!</p>
-            <p className="text-blue-400 font-bold pb-8 lg:pb-6 text-center md:text-left fade-in">Download our app:</p>
-            <div className="flex w-full justify-center md:justify-start pb-24 lg:pb-0 fade-in">
-              <img src="App Store.svg" className="h-12 pr-4 bounce-top-icons" />
-              <img src="Play Store.svg" className="h-12 bounce-top-icons" />
-            </div>
+            <h1 className="my-4 text-3xl md:text-5xl text-purple-800 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Ta com tempo? Aprenda no tap!</h1>
+            <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">Tap Skill, a maneira mais fácil de usar seu tempo livre para aprender uma nova habilidade ! Tap Aqui!</p>
           </div>
           <div className="w-full xl:w-3/5 py-6 overflow-y-hidden">
-            <img className="w-5/6 mx-auto lg:mr-0 slide-in-bottom" src="/images/devices.svg" />
-            <Image src="/images/devices.svg" />
+            <img className="w-5/6 mx-auto lg:mr-0 slide-in-bottom" src="devices.svg" />
           </div>
           
         </div>
